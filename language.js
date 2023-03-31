0594f0aa94f9ea08b82aacf124bec2e4430b28c4
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2023-03-31 14:00:12 +0530

    Create language.js

---
+++ language.js
@@ -0,0 +1,26 @@
+const Config = require('./settings');
+const fs = require('fs');
+const chalk = require('chalk');
+
+if (fs.existsSync('./src/' + Config.LANG + '.json')) {
+    console.log(
+        chalk.green.bold('Loading ' + Config.LANG + ' language...')
+    );
+
+    var json = JSON.parse(fs.readFileSync('./src/' + Config.LANG + '.json'));
+} else {
+    console.log(
+        chalk.red.bold('You entered an invalid language. English language was chosen.')
+    );
+
+    var json = JSON.parse(fs.readFileSync('./src/EN.json'));
+}
+
+function getString(file) {
+    return json['STRINGS'][file];
+}
+
+module.exports = {
+    language: json,
+    getString: getString
+}
