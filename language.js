ff22ac45feb6dac9d3e066dbec25289efcdf5b4a
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2023-04-01 18:04:34 +0530

    Update language.js

--- language.js
+++ language.js
@@ -2,11 +2,11 @@ require('./settings');
 const fs = require('fs');
 const chalk = require('chalk');
 
-if (fs.existsSync('./src/' + global.mess + '.json')) {
+if (fs.existsSync('./src/language/'+global.mess+'.json')) {
     console.log(
-        chalk.green.bold('Loading ' + global.mess + 'language...')
+        chalk.green.bold('Loading' + global.mess + 'language...')
     );
-    var json = JSON.parse(fs.readFileSync('./src/language/' + global.mess + '.json'));
+    var json = JSON.parse(fs.readFileSync('./src/language/'+ global.mess+'.json'));
 } else {
     console.log(
         chalk.red.bold('You entered an invalid language. English language was chosen.')
